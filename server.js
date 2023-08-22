f8b2f67a5faf46db7ab9c9c3c9f330dfdd45b371
ZakharTsarapov <tsarapovs@gmail.com>
2023-08-22 19:12:35 +0300

    start

---
+++ server.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const {DB_HOST, PORT} = process.env;
+
+mongoose.connect(DB_HOST)
+.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running. Use our API on port: ${PORT}`)
+  })
+})
+.catch(error => {
+  process.exit(1);
+})
