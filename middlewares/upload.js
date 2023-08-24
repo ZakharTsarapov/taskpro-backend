eb8ab28f51e4a5879ea0daa3785790e2eb9cb85d
K-Basket <koshykkv@gmail.com>
2023-08-24 19:38:54 +0300

    add updateAvatar

---
+++ middlewares/upload.js
@@ -0,0 +1,23 @@
+import multer from 'multer';
+import path from 'path';
+
+const tempDir = path.resolve('temp');
+
+const multerConfig = multer.diskStorage({
+  destination: tempDir,
+  filename: (req, file, cb) => {
+    const uniquePrefix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    const fileName = `${uniquePrefix}_${file.originalname}`;
+
+    cb(null, fileName);
+  },
+});
+
+const limits = {
+  fileSize: 1024 * 1024 * 5,
+};
+
+export const upload = multer({
+  storage: multerConfig,
+  limits,
+});
--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import authCtrl from '../controllers/authCtrl';
 const authRouter = express.Router();
-import { authenticate } from '../../middlewares/index.js';
+import { authenticate, isEmptyBody } from '../../middlewares/index.js';
+import { upload } from '../../middlewares/upload.js';
+import updateAvatar from '../../controllers/authCtrl/updateAvatar';
 
 authRouter.get('/current', authenticate, authCtrl.getCurrent);
+
+authRouter.post('/avatar', isEmptyBody, upload.single('avatar'), updateAvatar); // {avatar: avatar.jpg} - поле от фронта
 
 export default authRouter;
