a3c40113c803c52426a812f7ef7e521559f90019
ZakharTsarapov <tsarapovs@gmail.com>
2023-08-24 23:59:02 +0300

    shcemas

--- shemas/users-schemas.js
+++ shemas/users-schemas.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 import { emailRegExp, passwordRegExp, nameRegExp, deadlineRegExp } from '../constants/regExp.js';
 
 const userSignupSchema = Joi.object({
-  name: Joi.string().pattern(nameRegExp).required,
+  name: Joi.string().pattern(nameRegExp).required(),
   password: Joi.string().pattern(passwordRegExp).required(),
   email: Joi.string().pattern(emailRegExp).required(),
 });
