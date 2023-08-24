24657f97fe74b3f05a5afdf066b44b3ac016805a
OlgaTikhonchik <olga882011@gmail.com>
2023-08-24 22:47:27 +0300

    Update authRoutes.js

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-// import authCtrl from '../../controllers/authCtrl';
+import authCtrl from '../../controllers/authCtrl/getCurrent.js';
 import { authenticate, isEmptyBody, upload } from '../../middlewares/index.js';
 import authControllers from '../../controllers/authCtrl/authControllers.js';
 import usersSchemas from '../../shemas/users-schemas.js';
@@ -8,7 +8,7 @@ import updateAvatar from '../../controllers/authCtrl/updateAvatar.js';
 
 const authRouter = express.Router();
 
-// authRouter.get('/current', authenticate, authCtrl.getCurrent);
+authRouter.get('/current', authenticate, authCtrl.getCurrent);
 
 authRouter.post('/signup', validateBody(usersSchemas.userSignupSchema), authControllers.signup);
 
