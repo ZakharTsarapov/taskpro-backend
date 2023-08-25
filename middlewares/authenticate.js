4df0c21fea9385a4c5ea3320acbb7e1083da13e8
ZakharTsarapov <tsarapovs@gmail.com>
2023-08-25 16:27:49 +0300

    JWT_SECRET_KEY

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -16,7 +16,7 @@ const authenticate = async (req, res, next) => {
   }
 
   try {
-    const { id } = jwt.verify(token, JWT_SECRET);
+    const { id } = jwt.verify(token, JWT_SECRET_KEY);
     const user = await User.findById(id);
     if (!user || !user.token) {
       throw HttpError(401, 'Not authorized');
