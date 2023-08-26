5a242bc90dcf2d5f768a1a9973bb36b21efb8e69
OlgaTikhonchik <olga882011@gmail.com>
2023-08-26 15:28:49 +0300

    Update index.js

--- controllers/boards/index.js
+++ controllers/boards/index.js
@@ -1,2 +1,5 @@
 export { default as addBoard } from './addBoard.js';
 export { default as deleteBoard } from './deleteBoard.js';
+export { default as updateBoard } from './updateBoard.js';
+export { default as getAllBoards } from './getAllBoards.js';
+export { default as getBoardById } from './getBoardById.js';
