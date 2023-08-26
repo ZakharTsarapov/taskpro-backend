aef65b2de916eaef2eff3ce97b14684266a18719
OlgaTikhonchik <olga882011@gmail.com>
2023-08-26 15:28:53 +0300

    Create getBoardById.js

---
+++ controllers/boards/getBoardById.js
@@ -0,0 +1,24 @@
+import { Board } from '../../models/board-model.js';
+import { ctrlWrapper } from '../../decorators/index.js';
+import { HttpError } from '../../helpers/index.js';
+
+const getBoardById = async (req, res) => {
+  const { id } = req.params;
+
+  const { _id: owner } = req.user;
+  const result = await Board.findById({ _id: id, owner });
+
+  if (!result) {
+    throw HttpError(404, 'Not found');
+  }
+
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      result,
+    },
+  });
+};
+
+export default { getBoardById: ctrlWrapper(getBoardById) };
