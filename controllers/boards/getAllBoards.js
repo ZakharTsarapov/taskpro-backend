248f05c8dc089c5abee97dd7086f1db7a54156ca
OlgaTikhonchik <olga882011@gmail.com>
2023-08-26 15:28:56 +0300

    Create getAllBoards.js

---
+++ controllers/boards/getAllBoards.js
@@ -0,0 +1,16 @@
+import { Board } from '../../models/board-model.js';
+import { ctrlWrapper } from '../../decorators/index.js';
+
+const getAllBoards = async (req, res) => {
+  const { _id: owner } = req.user;
+
+  const result = await Board.find({ owner }, '-createdAt -updatedAt').populate('owner', 'title icon background ');
+  res.json({
+    status: 'success',
+    code: 200,
+    message: 'Boards successfully received',
+    boards: result,
+  });
+};
+
+export default { getAllBoards: ctrlWrapper(getAllBoards) };
