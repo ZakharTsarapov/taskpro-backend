fd5a1155a5c2ee67a569f77b9e978e4425f48881
OlgaTikhonchik <olga882011@gmail.com>
2023-08-28 21:48:13 +0300

    Update column-model.js

--- models/column-model.js
+++ models/column-model.js
@@ -9,9 +9,10 @@ const columnSchema = new Schema(
     },
     board: {
       type: Schema.Types.ObjectId,
-      required: [true, 'Board assignment for the column is required'],
+      //required: [true, 'Board assignment for the column is required'],
       ref: 'board',
     },
+
     cardOrder: {
       type: Array,
       default: [],
