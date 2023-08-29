175f24efd63c915659b6a015e916a6503cfa89f3
OlgaTikhonchik <olga882011@gmail.com>
2023-08-29 12:43:38 +0300

    Update index.js

--- controllers/columns/index.js
+++ controllers/columns/index.js
@@ -1,3 +1,3 @@
 export { default as addColumn } from './addColumn.js';
-// export { default as deleteColumn } from './deleteColumn.js';
-// export { default as updateColumn } from './updateColumn.js';
+export { default as deleteColumn } from './deleteColumn.js';
+export { default as updateColumn } from './updateColumn.js';
