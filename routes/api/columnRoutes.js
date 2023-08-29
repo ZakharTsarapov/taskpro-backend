19697be7890c7ba0b62cf9d35c05d7895a0aafda
OlgaTikhonchik <olga882011@gmail.com>
2023-08-29 12:43:19 +0300

    Update columnRoutes.js

--- routes/api/columnRoutes.js
+++ routes/api/columnRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { isValidId, authenticate } from '../../middlewares/index.js';
-import { addColumn } from '../../controllers/columns/index.js';
+import { addColumn, deleteColumn, updateColumn } from '../../controllers/columns/index.js';
 
 const columnRouter = express.Router();
 
 columnRouter.post('/', authenticate, addColumn.addColumn);
-// columnRouter.delete('/:id', authenticate, isValidId, deleteColumn.deleteColumn);
-// columnRouter.put('/:id', authenticate, isValidId, updateColumn.updateColumn);
+columnRouter.delete('/:id', authenticate, isValidId, deleteColumn.deleteColumn);
+columnRouter.put('/:id', authenticate, isValidId, updateColumn.updateColumn);
 
 export default columnRouter;
