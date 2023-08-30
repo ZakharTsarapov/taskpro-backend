90cb9c147a9de0de863964bc37c4f9af0dbe0f68
ZakharTsarapov <tsarapovs@gmail.com>
2023-08-30 15:44:55 +0300

    update

--- controllers/columns/getAllColumns.js
+++ controllers/columns/getAllColumns.js
@@ -3,14 +3,14 @@ import Column from '../../models/column-model.js';
 import { ctrlWrapper } from '../../decorators/index.js';
 
 const getAllColumns = async (req, res) => {
-  const { column } = req.params;
+  const { _id: owner } = req.user;
 
-  const result = await Column.find({ column }, '-createdAt -updatedAt').populate('board', 'title ');
+  const result = await Column.find({ owner }, '-createdAt -updatedAt').populate('board', 'title ');
   res.json({
     status: 'success',
     code: 200,
     message: 'Columns successfully received',
-    columns: result,
+    result,
   });
 };
 
