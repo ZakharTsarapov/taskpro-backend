e2cdec1002f0c46ffb8cfa5729408c30205ec959
ZakharTsarapov <tsarapovs@gmail.com>
2023-08-30 15:23:24 +0300

    fix

--- controllers/boards/getAllBoards.js
+++ controllers/boards/getAllBoards.js
@@ -2,14 +2,14 @@ import Board from '../../models/board-model.js';
 import { ctrlWrapper } from '../../decorators/index.js';
 
 const getAllBoards = async (req, res) => {
-  const { board } = req.params;
+  const { _id: owner } = req.user;
 
-  const result = await Board.find({ board }, '-createdAt -updatedAt').populate('owner', 'title icon background ');
+  const result = await Board.find({ owner }, '-createdAt -updatedAt').populate('owner', 'title icon background ');
   res.json({
     status: 'success',
     code: 200,
     message: 'Boards successfully received',
-    boards: result,
+    result,
   });
 };
 
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -18,7 +18,7 @@ const authenticate = async (req, res, next) => {
   try {
     const { id } = jwt.verify(token, JWT_SECRET_KEY);
     const user = await User.findById(id);
-    if (!user || !user.token) {
+    if (!user || !user.token || user.token !== token ) {
       throw HttpError(401, 'Not authorized');
     }
     req.user = user;
