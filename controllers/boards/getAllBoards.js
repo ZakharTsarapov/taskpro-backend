89f433687fde70d2ce6b5ca5b6e93ce989536eca
OlgaTikhonchik <olga882011@gmail.com>
2023-08-30 14:35:34 +0300

    Update getAllBoards.js

--- controllers/boards/getAllBoards.js
+++ controllers/boards/getAllBoards.js
@@ -2,7 +2,6 @@ import Board from '../../models/board-model.js';
 import { ctrlWrapper } from '../../decorators/index.js';
 
 const getAllBoards = async (req, res) => {
-  const { _id: owner } = req.user;
   const { board } = req.params;
 
   const result = await Board.find({ board }, '-createdAt -updatedAt').populate('owner', 'title icon background ');
