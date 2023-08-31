dfc915f3916af5ba2cbf5c10fef0418ee8cd67ae
ZakharTsarapov <tsarapovs@gmail.com>
2023-08-31 09:42:37 +0300

    swagger columns

--- routes/api/boardRoutes.js
+++ routes/api/boardRoutes.js
@@ -4,10 +4,12 @@ import { isValidId, authenticate } from '../../middlewares/index.js';
 
 const boardRouter = express.Router();
 
-boardRouter.post('/', authenticate, addBoard.addBoard);
-boardRouter.delete('/:id', authenticate, isValidId, deleteBoard.deleteBoard);
-boardRouter.put('/:id', authenticate, isValidId, updateBoard.updateBoard);
-boardRouter.get('/', authenticate, getAllBoards.getAllBoards);
-boardRouter.get('/:id', authenticate, getBoardById.getBoardById);
+boardRouter.use(authenticate);
+
+boardRouter.post('/', addBoard.addBoard);
+boardRouter.delete('/:id', isValidId, deleteBoard.deleteBoard);
+boardRouter.put('/:id', isValidId, updateBoard.updateBoard);
+boardRouter.get('/', getAllBoards.getAllBoards);
+boardRouter.get('/:id', getBoardById.getBoardById);
 
 export default boardRouter;
