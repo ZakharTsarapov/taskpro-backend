912ce203055c84e4d6181dbbd02d608b03884f09
OlgaTikhonchik <olga882011@gmail.com>
2023-08-31 20:26:45 +0300

    Update authRoutes.js

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -20,10 +20,10 @@ authRouter.post('/signout', authenticate, authControllers.signout);
 
 authRouter.post('/avatar', authenticate, upload.single('avatar'), updateAvatar.updateAvatar);
 
-// authRouter.put(':id/update', authenticate, upload.single('avatar'), updateCtrl.updateDataUser);
 authRouter.put('/update', authenticate, upload.single('avatar'), updateCtrl.updateDataUser);
-//authRouter.patch('/update', authenticate, updateCtrl.updateDataUser);
 
 authRouter.patch('/themes', validateBody(usersSchemas.userChangeTheme), authenticate, changeTheme.changeTheme);
+
+authRouter.post('/refresh', validateBody(usersSchemas.refreshSchema), authControllers.refresh);
 
 export default authRouter;
