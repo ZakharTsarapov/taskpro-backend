febcc0b22048c69d2ff280e898c5b5b1807e11a0
ZakharTsarapov <tsarapovs@gmail.com>
2023-09-01 19:34:38 +0300

    yeeee

--- routes/api/columnRoutes.js
+++ routes/api/columnRoutes.js
@@ -5,7 +5,7 @@ import {
   deleteColumn,
   updateColumn,
   getAllColumns,
-  getColumnById,
+  // getColumnById,
 } from '../../controllers/columns/index.js';
 
 const columnRouter = express.Router();
@@ -15,5 +15,5 @@ columnRouter.post('/', authenticate, addColumn.addColumn);
 columnRouter.delete('/:id', authenticate, isValidId, deleteColumn.deleteColumn);
 columnRouter.put('/:id', authenticate, isValidId, updateColumn.updateColumn);
 columnRouter.get('/:id', authenticate, getAllColumns.getAllColumns); // id board
-columnRouter.get('/:id', authenticate, getColumnById.getColumnById);
+// columnRouter.get('/:id', authenticate, getColumnById.getColumnById);
 export default columnRouter;
