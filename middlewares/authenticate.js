a8f88a151e17548a6433ba60c8bc91cd72858f53
OlgaTikhonchik <olga882011@gmail.com>
2023-09-03 17:14:47 +0300

    update token

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,24 +1,24 @@
-import jwt from "jsonwebtoken";
-import { ctrlWrapper } from "../decorators/index.js";
-import {HttpError} from "../helpers/index.js";
-import User from "../models/user-model.js";
+import jwt from 'jsonwebtoken';
+import { ctrlWrapper } from '../decorators/index.js';
+import { HttpError } from '../helpers/index.js';
+import User from '../models/user-model.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const { JWT_SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
-  const { authorization = "" } = req.headers;
-  const [bearer, token] = authorization.split(" ");
+  const { authorization = '' } = req.headers;
+  const [bearer, token] = authorization.split(' ');
 
-  if (bearer !== "Bearer") {
+  if (bearer !== 'Bearer') {
     throw HttpError(401, 'Not authorized');
   }
 
   try {
     const { id } = jwt.verify(token, JWT_SECRET_KEY);
     const user = await User.findById(id);
-    if (!user || !user.token || user.token !== token ) {
+    if (!user || !user.token || user.token !== token) {
       throw HttpError(401, 'Not authorized');
     }
     req.user = user;
@@ -29,4 +29,3 @@ const authenticate = async (req, res, next) => {
 };
 
 export default ctrlWrapper(authenticate);
-
