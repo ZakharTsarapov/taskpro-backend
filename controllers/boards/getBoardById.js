8df185e79148126abd178c1109fe159fa735bc3a
ZakharTsarapov <tsarapovs@gmail.com>
2023-09-03 20:42:59 +0300

    aggregate boards

--- controllers/boards/getBoardById.js
+++ controllers/boards/getBoardById.js
@@ -1,4 +1,5 @@
 import  Board  from '../../models/board-model.js';
+import Column from "../../models/column-model.js"
 import { ctrlWrapper } from '../../decorators/index.js';
 import { HttpError } from '../../helpers/index.js';
 
@@ -8,6 +9,25 @@ const getBoardById = async (req, res) => {
   const { _id: owner } = req.user;
   const result = await Board.findById({ _id: id, owner });
 
+  const columns = await Column.find({ board: result._id });
+  let columnsWithCards = [];
+
+  if (columns.length > 0) {
+    columnsWithCards = await Column.aggregate([
+      {
+        $match: { $or: columns },
+      },
+      {
+        $lookup: {
+          from: "tasks",
+          localField: "_id",
+          foreignField: "board",
+          as: "tasks",
+        },
+      },
+    ]);
+  }
+
   if (!result) {
     throw HttpError(404, 'Not found');
   }
@@ -15,9 +35,8 @@ const getBoardById = async (req, res) => {
   res.json({
     status: 'success',
     code: 200,
-    data: {
-      result,
-    },
+    result,
+    columns: columnsWithCards,
   });
 };
 
