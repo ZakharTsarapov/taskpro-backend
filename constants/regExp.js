ba37617c7f39d785506977dbb94463d06a3a4d5e
ZakharTsarapov <tsarapovs@gmail.com>
2023-10-05 10:44:57 +0300

    nameRegExp

--- constants/regExp.js
+++ constants/regExp.js
@@ -1,4 +1,4 @@
 export const emailRegExp = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/;
 export const passwordRegExp = /^[a-zA-Z0-9\-!@#$%^&*()_+,.:;'"?/]+$/;
-export const nameRegExp = /^[a-zA-Z0-9 !@#$%^&*()_+,.:;'"?/-]+$/;
+export const nameRegExp = /^[a-zA-Z0-9а-яА-ЯёЁіІїЇєЄґҐ !@#$%^&*()_+,.:;'"?/-]+$/;
 export const deadlineRegExp = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
