65811d60282ae8d1bf69a540190b92b80ddb690e
ZakharTsarapov <tsarapovs@gmail.com>
2023-10-10 11:29:57 +0300

    regexp

--- constants/regExp.js
+++ constants/regExp.js
@@ -1,4 +1,4 @@
-export const emailRegExp = /^([a-zA-Z0-9_-]+\.)*[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/
-export const passwordRegExp = /^[0-9a-zA-Zа-яА-ЯёЁіІїЇєЄґҐ!@#$%^&* .\\/-]{6,32}$/
+export const emailRegExp = /^([a-zA-Z0-9_-]+\.)*[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
+export const passwordRegExp = /^[0-9a-zA-Zа-яА-ЯёЁіІїЇєЄґҐ!@#$%^&* .\\/-]{6,32}$/;
 export const nameRegExp = /^[0-9a-zA-Zа-яА-ЯёЁіІїЇєЄґҐ!@#$%^&* .\\/-]{2,32}$/;
 export const deadlineRegExp = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
