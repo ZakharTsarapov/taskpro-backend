b0cc27c98241e940559bfa68858cab404e305d2c
ZakharTsarapov <tsarapovs@gmail.com>
2023-10-25 16:40:16 +0300

    fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`💙💛 Server running. Use our API on port: ${PORT}`);
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
   })
   .catch(error => {
